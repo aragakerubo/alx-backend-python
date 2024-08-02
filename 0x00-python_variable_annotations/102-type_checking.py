9e33eb6dc35e5f11409583209b040df26b57ffe5
aragakerubo <aragakerubo@gmail.com>
2024-08-02 12:04:17 -0400

    12. Type Checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 """Type-annotated function type_checking with mypy which takes
 a variable x of type Tuple and returns the sum as a float."""
-from typing import Tuple, Union
+
+from typing import Tuple, List
 
 
-def type_checking(n: Union[int, float]) -> Union[int, float]:
-    """Return the sum of a tuple of floats."""
-    return n
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Return a list of tuples, each with a sequence and its length."""
+    zoomed_in: List = [item for item in lst for i in range(factor)]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3.0)
