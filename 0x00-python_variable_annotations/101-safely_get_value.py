1de2db7779482bb1b477315d52a9d1cb99f05bbe
aragakerubo <aragakerubo@gmail.com>
2024-08-02 12:01:48 -0400

    11. More involved type annotations

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-"""Type-annotated function safely_get_value that takes a dict input_dict of
-type Mapping, a key key of type Any, a default value default of type Union[T, None],
-and returns the value with key key in input_dict if key is present, or default."""
+"""Type-annotated function safely_get_value that takes a
+dict input_dict of type Mapping, a key key of type Any,
+a default value default of type Union[T, None], and
+returns the value with key key in input_dict if key is present, or default."""
 
 from typing import Mapping, Any, Union, TypeVar
 
@@ -11,7 +12,8 @@ T = TypeVar("T")
 def safely_get_value(
     dct: Mapping, key: Any, default: Union[T, None] = None
 ) -> Union[Any, T]:
-    """Return the value with key key in input_dict if key is present, or default."""
+    """Return the value with key key in input_dict if key is present,
+    or default."""
     if key in dct:
         return dct[key]
     else:
