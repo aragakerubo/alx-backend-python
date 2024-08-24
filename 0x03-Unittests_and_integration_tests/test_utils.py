14591935731d870b8da6c568b5215d3bedd53e16
aragakerubo <aragakerubo@gmail.com>
2024-08-24 19:31:20 -0400

    3. Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -49,3 +49,45 @@ class TestGetJson(unittest.TestCase):
 
         self.assertEqual(get_json(test_url), test_payload)
         mock_requests_get.assert_called_once_with(test_url)
+
+
+# Read about memoization and familiarize yourself with the utils.memoize decorator.
+
+# Implement the TestMemoize(unittest.TestCase) class with a test_memoize method.
+
+# Inside test_memoize, define following class
+
+# class TestClass:
+
+#     def a_method(self):
+#         return 42
+
+#     @memoize
+#     def a_property(self):
+#         return self.a_method()
+# Use unittest.mock.patch to mock a_method. Test that when calling a_property twice, the correct result is returned but a_method is only called once using assert_called_once.
+
+
+class TestMemoize(unittest.TestCase):
+    """TestMemoize"""
+
+    @patch("utils.get_json")
+    def test_memoize(self, mock_get_json):
+        """Test memoize"""
+
+        class TestClass:
+            """TestClass"""
+
+            def a_method(self):
+                """a_method"""
+                return 42
+
+            @memoize
+            def a_property(self):
+                """a_property"""
+                return self.a_method()
+
+        test = TestClass()
+        self.assertEqual(test.a_property, 42)
+        self.assertEqual(test.a_property, 42)
+        mock_get_json.assert_called_once()
