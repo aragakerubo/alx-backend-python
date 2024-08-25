d317c7dd98e205e9f44d88772e895fca9c097932
aragakerubo <aragakerubo@gmail.com>
2024-08-24 20:19:35 -0400

    3. Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -55,23 +55,28 @@ class TestMemoize(unittest.TestCase):
     """TestMemoize"""
 
     @patch.object("TestClass", "a_method")
-    def test_memoize(self, mock_get_json):
-        """Test memoize"""
+    def test_memoize(self, mock_a_method):
+        """Test that when calling a_property twice, the correct result
+        is returned but a_method is only called once using
+        assert_called_once
+        """
 
         class TestClass:
-            """TestClass"""
+            """TestClass that was provided"""
 
             def a_method(self):
-                """a_method"""
+                """method return value"""
                 return 42
 
             @memoize
             def a_property(self):
-                """a_property"""
+                """property returns value"""
                 return self.a_method()
 
-        test = TestClass()
-        mock_get_json.return_value = 42
-        test.a_property()
-        test.a_property()
-        mock_get_json.assert_called_once()
+        test_instance = TestClass()
+        mock_a_method.return_value = 42
+
+        test_instance.a_property()
+        test_instance.a_property()
+
+        mock_a_method.assert_called_once()
